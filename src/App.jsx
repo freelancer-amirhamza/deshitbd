0928d44e489931dfcb113976b01bd749bfaf9ad9
Amir Hamza <amirhamza27940@gmail.com>
2025-09-13 07:08:43 +0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className="text-8xl font-extrabold text-lime-600">Welcome to Desh IT </h1>
+    </>
+  )
+}
+
+export default App
